a4418510e9d7a1b935be215540b0a647e5f0e82f
hrq <hrq@servyou.com.cn>
2020-06-11 14:03:38 +0800

    测试路由功能：通过<a></a>标签进行页面间的路由切换

--- src/test_router/router_pages/detail.js
+++ src/test_router/router_pages/detail.js
@@ -1,11 +1,14 @@
-import React from 'react';
 //测试路由功能
+import React from 'react';
+
+//目标地址,通过a标签进行跳转
+let forward = '#/'
 
 export default class Home extends React.Component {
     render() {
         return (
             <div>
-                <a>回到home</a>
+                <a href={forward}>回到home</a>
             </div>
         )
     }
--- src/test_router/router_pages/home.js
+++ src/test_router/router_pages/home.js
@@ -1,11 +1,14 @@
+//测试路由功能
 import React from 'react';
 
-//测试路由功能
+//目标地址,通过a标签进行跳转
+let target = '#/detail';
+
 export default class Home extends React.Component {
     render() {
         return (
             <div>
-                <a>去detail</a>
+                <a href={target}>去detail</a>
             </div>
         )
     }
