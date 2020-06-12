570d2dbd85efff802b0f06d66331110a2c5d988b
hrq <hrq@servyou.com.cn>
2020-06-12 14:36:17 +0800

    index.js中测试: hook的useEffect 相当于 componentDidMount 和 componentDidUpdate

--- src/hook/HookExample.js
+++ src/hook/HookExample.js
@@ -1,7 +1,11 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 function HookExample() {
     //声明一个新的叫做count的变量
     const [count, setCount] = useState(0);
+    useEffect(() => {
+        //相当于 componentDidMount 和 componentDidUpdate
+        document.title = 'You clicked ' + count + 'times!';
+    });
     return (
         <div>
             <p>
